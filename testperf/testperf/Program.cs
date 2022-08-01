657200d0e3627a3aea5e15023c051493029ba8a1
Utkarsh Gohil <52894622+utkarshgohil@users.noreply.github.com>
2022-08-01 18:30:54 +0530

    Add files via upload

---
+++ testperf/testperf/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testperf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> li = new List<int>(1000);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            for(int i = 0; i < 1000; i++)
+            {
+                li.Add(i);
+            }
+            sw.Stop();
+            Console.WriteLine("forloop " + sw.ElapsedTicks);
+            sw.Reset();
+
+            sw.Start();
+
+            foreach (int i in li)
+            {
+                //Console.WriteLine(+i);
+            }
+            sw.Stop();
+            Console.WriteLine("foreachloop " + sw.ElapsedTicks);
+            
+        }
+    }
+}
