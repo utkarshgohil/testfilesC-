aebd526ee0d3ff4f49ac636caa9cb7fb0de446ab
Utkarsh Gohil <52894622+utkarshgohil@users.noreply.github.com>
2022-08-01 18:28:26 +0530

    Add files via upload

---
+++ testoperatorover/testoperatorover/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testoperatorover
+{
+    public class Cpoint
+    {
+        int x, y;
+        public Cpoint()
+        {
+            x = 10;
+            y = 20;
+        }
+        public Cpoint (int i,int j)
+        {
+            x = i;
+            y = j;
+        }
+        public int X
+        {
+            set
+            {
+                x = value;
+            }
+            get
+            {
+                return x;
+            }
+        }
+        public int Y
+        {
+            set
+            {
+                y = value;
+            }
+            get
+            {
+                return y;
+            }
+        }
+
+        public void prnt()
+        {
+            Console.WriteLine("value of x & y "+x+" "+y);
+        }
+        public static Cpoint operator+(Cpoint p1,Cpoint p2)
+        {
+            Cpoint temp=new Cpoint();
+            temp.x = p1.x + p2.x;
+            temp.y = p1.y + p2.y;
+            return temp;
+        }
+        public static Cpoint operator ++(Cpoint p1)
+        {
+            p1.x++;
+            p1.y++;
+            return p1;
+        }
+        public static bool operator >(Cpoint p1,Cpoint p2)
+        {
+            if (p1.x > p2.x && p1.y > p2.y)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public static bool operator <(Cpoint p1, Cpoint p2)
+        {
+            if (p1.x < p2.x && p1.y < p2.y)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public static implicit operator Cpoint(int i)
+        {
+            Cpoint t = new testoperatorover.Cpoint(i, 10);
+            return t;
+        }
+        public static explicit operator double(Cpoint c)
+        {
+            return (double)c.X;
+        }
+    }
+}
---
+++ testoperatorover/testoperatorover/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testoperatorover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cpoint p1 = new Cpoint();
+            p1.X = 10;
+            p1.Y = 20;
+            Cpoint p2 = new Cpoint(30,40);
+            p1.prnt();
+            p2.prnt();
+            Cpoint p3 = new Cpoint();
+            p3 = p1 + p2;
+            p3.prnt();
+
+            p1 = 100;
+            p1.prnt();
+            double d = (double)p1;
+            Console.WriteLine(d);
+        }
+    }
+}
