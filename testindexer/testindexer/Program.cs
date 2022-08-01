8e7fd1a805533b6396248b2f97e86da799ea5579
Utkarsh Gohil <52894622+utkarshgohil@users.noreply.github.com>
2022-08-01 18:27:40 +0530

    Add files via upload

---
+++ testindexer/testindexer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testindexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testind t=new testind();
+            t[0] = "abc";
+            t[1] = "xyz";
+            Console.WriteLine(t[0]);
+            Console.WriteLine(t[1]);
+        }
+    }
+}
---
+++ testindexer/testindexer/testind.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testindexer
+{
+    class testind
+    {
+        int[] arr = new int[5];
+        int age;
+        string [] arr1 = new string[5];
+        //public int this[int index]
+        //{
+        //    set
+        //    {
+        //        arr[index] = value;
+        //    }
+        //    get
+        //    {
+        //        return arr[index];
+        //    }
+        //}
+        public string this[int s]
+        {
+            set
+            {
+                arr1[s] = value;
+            }
+            get
+            {
+                return arr1[s];
+            }
+        }
+        //public int AGE
+        //{
+
+        //}
+    }
+}
